841caabcc1c8d38399963c4cef83c8dc97c4abf9
Finn Ellis <75760736+finn-ellis@users.noreply.github.com>
2021-11-30 15:32:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ box_sheet_range = ''
 
 def init_box():
   box_id = sheets_talker.worksheet.row_count+1
-  print(box_id)
   box_sheet_range = 'A' + str(box_id) + ':B' + str(box_id)
-  print(box_sheet_range)
   sheets_talker.worksheet.add_rows(1) #add a new row for the box
 
 init_box()
+print(box_id, box_sheet_range)
 sheets_talker.worksheet.update(box_sheet_range, [[str(box_id), str(risk)]])
