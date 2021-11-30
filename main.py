5c0e83748da028ca23a66f67f003bed4febe53c1
Finn Ellis <75760736+finn-ellis@users.noreply.github.com>
2021-11-30 15:34:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ humidity = 0 / 100 #We want humidity as a decimal
 smoke = 0 #Remember that smoke is measured in output on a 3.3V scale
 risk = temperature - (temperature * humidity)
 
-box_id = 0
-box_sheet_range = ''
-
 def init_box():
-  global box_id = sheets_talker.worksheet.row_count+1
-  global box_sheet_range = 'A' + str(box_id) + ':B' + str(box_id)
+  box_id = sheets_talker.worksheet.row_count+1
+  box_sheet_range = 'A' + str(box_id) + ':B' + str(box_id)
   sheets_talker.worksheet.add_rows(1) #add a new row for the box
+  return box_id, box_sheet_range
 
-init_box()
+box_id. box_sheet_range = init_box()
 print(box_id, box_sheet_range)
 sheets_talker.worksheet.update(box_sheet_range, [[str(box_id), str(risk)]])
