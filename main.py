65ef7d286b7e2efdc8afc80765bf6ef586196158
Finn Ellis <75760736+finn-ellis@users.noreply.github.com>
2021-11-30 15:15:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sheets_talker
 #Analyze Data
 #Variable values will be replaced by the values from the sensors
 temperature = 0 #Fahrenheit
 humidity = 0 / 100 #We want humidity as a decimal
 smoke = 0 #Remember that smoke is measured in output on a 3.3V scale
 risk = temperature - (temperature * humidity)
+
+sheets_talker.set_Values('A1', str(risk))
