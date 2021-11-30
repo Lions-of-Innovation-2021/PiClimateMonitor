71fabfa6ef55ff8fa9d9db965843d4750c1d21ea
Finn Ellis <75760736+finn-ellis@users.noreply.github.com>
2021-11-30 15:01:41 -0700

    Create sheets_talker.py
    Code for updating google sheet

---
+++ sheets_talker.py
@@ -0,0 +1,26 @@
+# Talks to google sheets
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#Authorize the API
+scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+file_name = 'client_key.json'
+creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+client = gspread.authorize(creds)
+
+worksheet = client.open('Sheet Name Here').sheet1
+
+# read a cell
+cell = worksheet.cell(0, 0) #row, col
+print(cell.value)
+
+worksheet.get('A1') # works with multiple cells
+
+# update a cell
+worksheet.update_cell(0, 0, 'Text') #row, col
+worksheet.update_acell('A1', 'Text') #works with one cell
+
+worksheet.update('A1', 'Hello') # works with multiple cells
