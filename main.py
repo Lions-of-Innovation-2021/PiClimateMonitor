99e1954b349270240b866cde9b01d074030c7255
Finn Ellis <75760736+finn-ellis@users.noreply.github.com>
2021-11-30 15:16:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ humidity = 0 / 100 #We want humidity as a decimal
 smoke = 0 #Remember that smoke is measured in output on a 3.3V scale
 risk = temperature - (temperature * humidity)
 
-sheets_talker.set_Values('A1', str(risk))
+sheets_talker.set_values('A1', str(risk))
