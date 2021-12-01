649eeebad812451d833388f6bc710d88e8240e0b
CharlesTChapman <charles.t.chapman@icloud.com>
2021-11-30 18:30:55 -0700

    Reads data from DHT22

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sheets_talker
+import Adafruit_DHT as dht
+from time import sleep
+ 
 #Analyze Data
 #Variable values will be replaced by the values from the sensors
 temperature = 0 #Fahrenheit
 humidity = 0 / 100 #We want humidity as a decimal
 smoke = 0 #Remember that smoke is measured in output on a 3.3V scale
-risk = temperature - (temperature * humidity)
 
 def init_box():
   box_id = sheets_talker.worksheet.row_count
@@ -12,6 +14,13 @@ def init_box():
   sheets_talker.worksheet.add_rows(1) #add a new row for the box
   return box_id, box_sheet_range
 
-box_id, box_sheet_range = init_box()
-print(box_id, box_sheet_range)
-sheets_talker.worksheet.update(box_sheet_range, [[str(box_id), str(risk)]])
+#Set data pin for DHT22
+DHT = 4
+while True:
+  #Read data from DHT22
+  humidity, temperature = dht.read_retry(dht.DHT22, DHT)
+  risk = temperature - (temperature * humidity)
+  box_id, box_sheet_range = init_box()
+  print(box_id, box_sheet_range)
+  sheets_talker.worksheet.update(box_sheet_range, [[str(box_id), str(risk)]])
+  sleep(5)
