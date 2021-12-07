f8c578580ee85b9c10c8f63de291fbefe9308ba9
= <xfinnellisx@gmail.com>
2021-12-07 13:31:35 -0700

    Gas sensor test

---
+++ gas_sensor_test.py
@@ -0,0 +1,17 @@
+import sys, time
+import gas_sensor.mq as mq
+
+try:
+    print("Press CTRL+C to abort.")
+    
+    MQ = mq.MQ();
+    while True:
+        perc = mq.MQPercentage()
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[K")
+        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+except:
+    print("\nAbort by user")
