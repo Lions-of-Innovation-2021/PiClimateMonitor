0b45e3907fe33e87e0419158086916e0673fc0b8
= <xfinnellisx@gmail.com>
2021-12-07 13:48:06 -0700

    updat

--- gas_sensor_test.py
+++ gas_sensor_test.py
@@ -1,17 +1,11 @@
 import sys, time, os
 from mq import MQ
 
-try:
-    print("Press CTRL+C to abort.")
-
-    mq = MQ()
-    while True:
-        perc = mq.MQPercentage()
-        sys.stdout.write("\r")
-        sys.stdout.write("\033[K")
-        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
-        sys.stdout.flush()
-        time.sleep(0.1)
-
-except:
-    print("\nAbort by user")
+mq = MQ()
+while True:
+    perc = mq.MQPercentage()
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+    sys.stdout.flush()
+    time.sleep(0.1)
