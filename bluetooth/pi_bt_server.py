aa39ad270d84bd6552d1455f555d9700c29cbd4b
= <xfinnellisx@gmail.com>
2021-12-09 14:48:43 -0700

    Bluetooth

--- bluetooth/pi_bt_server.py
+++ bluetooth/pi_bt_server.py
@@ -1,19 +1,33 @@
 import bluetooth
+import subprocess
+
+def receive_data(client_sock):
+    data = client_sock.recv(1024)
+    print("Received: [%s]" % data)
 
 def main():
+    subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+
     print("Hosting Pi bluetooth server...")
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     port = 1
     server_sock.bind(("",port))
-    server_sock.listen(1)   # can't find documentation on it
 
+    # begin listening for connection
+    print("Lisetning to port [%i]" % port)
+    server_sock.listen(port)   # can't find documentation on it
+
+    # wait for a connection from client
     client_sock, address = server_sock.accept()
-    print("Accepted connection from ", address)
+    device_name = bluetooth.lookup_name(address)
+    print(f"Accepted connection from {device_name} ({address})")
 
-    data = client_sock.recv(1024)
-    print("Received: [%s]" % data)
-
+    # receive data
+    while True:
+        receive_data(client_sock)
+    
+    # cleanup
     client_sock.close()
     server_sock.close()
 
