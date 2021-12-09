b6adfd0b791ca0fe68fe3d08cb6c0b0392df2d8b
= <xfinnellisx@gmail.com>
2021-12-09 14:37:58 -0700

    bt

---
+++ bluetooth/bt_client.py
@@ -0,0 +1,34 @@
+import bluetooth
+
+PI_NAME = "MIMS_STEM_RasPi"
+PI_ADDRESS = None
+
+def find_pi():
+    global PI_ADDRESS
+    nearby_devices = bluetooth.discover_devices()
+    for bdaddr in nearby_devices:
+        bdname = bluetooth.lookup_name(bdaddr)
+        print("Found:", bdname)
+        if bdname == PI_NAME:
+            PI_ADDRESS = bdaddr
+            break
+
+def main():
+    print("Searching for Pi Bluetooth...")
+    find_pi()
+    
+    if PI_ADDRESS:
+        print("Found Raspberry Pi")
+
+        port = 1
+
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        sock.connect((PI_ADDRESS, port))
+
+        sock.send("test data!")
+
+        sock.close()
+    else:
+        raise Exception("Raspberry PI not found!")
+
+main()
--- bluetooth/pi_bt_server.py
+++ bluetooth/pi_bt_server.py
@@ -1,16 +1,20 @@
 import bluetooth
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+def main():
+    print("Hosting Pi bluetooth server...")
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-port = 1
-server_sock.bind(("",port))
-server_sock.listen(1)   # can't find documentation on it
+    port = 1
+    server_sock.bind(("",port))
+    server_sock.listen(1)   # can't find documentation on it
 
-client_sock, address = server_sock.accept()
-print("Accepted connection from ", address)
+    client_sock, address = server_sock.accept()
+    print("Accepted connection from ", address)
 
-data = client_sock.recv(1024)
-print("received [%s]" % data)
+    data = client_sock.recv(1024)
+    print("Received: [%s]" % data)
 
-client_sock.close()
-server_sock.close()
+    client_sock.close()
+    server_sock.close()
+
+main()
